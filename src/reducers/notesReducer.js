7e17cfc029378c1155278e757f808c53e2763beb
Parker Helland <hellandp@gmail.com>
2019-06-10 11:17:41 -0700

    reversed order of pending and new note

--- src/reducers/notesReducer.js
+++ src/reducers/notesReducer.js
@@ -1,16 +1,16 @@
 import { NEW_NOTE, NEW_NOTE_PENDING } from '../actions/notesAction';
 
 const initialState = {
-  list: [],
-  loading: false
+  loading: false,
+  list: []
 };
 
 export default function reducer(state = initialState, action) {
-  switch(action.types) {
-    case NEW_NOTE: 
-      return { ...state, loading: false, list: [...state.list, action.payload] };
+  switch(action.type) {
     case NEW_NOTE_PENDING:
       return { ...state, loading: true };
+    case NEW_NOTE:
+      return { ...state, loading: false, list: [...state.list, action.payload] };
     default:
       return state;
   }

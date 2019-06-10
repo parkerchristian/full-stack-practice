9b5b0786c29bd99277c66b665369130d7563396a
Parker Helland <hellandp@gmail.com>
2019-06-10 10:28:02 -0700

    CreateNote container made. needs reducers/ actions

---
+++ src/containers/CreateNote.js
@@ -0,0 +1,43 @@
+import React, { PureComponent } from 'react';
+import PropTypes from 'prop-types';
+import NoteForm from '../components/notes/NoteForm';
+import { connect } from 'tls';
+
+class CreateNote extends PureComponent {
+  static propTypes = {
+    createNote: PropTypes.func.isRequired
+  }
+
+  state = {
+    title: '',
+    body: ''
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+    const { title, body } = this.state;
+    this.props.createNote({ title, body });
+  } 
+  handleChange = ({ target }) => {
+    this.setState({ [target.name]: target.value });
+  }
+
+  render() {
+    const { title, body } = this.state;
+    return (
+      <NoteForm onSubmit={this.handleSubmit} onChange={this.handleChange}
+        title={title} body={body} submitText='Create Note' />
+    );
+  }
+} 
+
+const mapDispatchToProps = dispatch => ({
+  createNote() {
+    dispatch();
+  }
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(CreateNote);

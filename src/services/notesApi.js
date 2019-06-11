11aea6f5c1021450386341e57b0e920d36e870c7
Parker Helland <hellandp@gmail.com>
2019-06-11 09:17:36 -0700

    update services to get notes

--- src/services/notesApi.js
+++ src/services/notesApi.js
@@ -1,4 +1,4 @@
-import { post } from './request';
+import { post, get } from './request';
 
 export const createNote = note => post('/api/v1/notes', note);
-
+export const getNote = () => get('/api/v1/notes');

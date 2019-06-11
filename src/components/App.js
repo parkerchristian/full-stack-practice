d6cd983ce9a35c100e6f8d9f139a6d1bd0357bdd
Parker Helland <hellandp@gmail.com>
2019-06-11 16:34:43 -0700

    update components and selectors

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
-import CreateNote from '../containers/CreateNote';
-import AllNotes from '../containers/AllNotes';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route
+} from 'react-router-dom';
+import Home from './Home';
+import { withSession } from '../containers/auth/withSession';
+import Callback from '../containers/auth/Callback';
 
 export default function App() {
   return (
-  <>
-    <CreateNote />
-    <AllNotes />
-  </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={withSession(Home)} />
+        <Route exact path="/callback" component={Callback} />
+      </Switch>
+    </Router>
   );
 }
--- src/components/App.test.js
+++ src/components/App.test.js
@@ -2,10 +2,11 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
 
+jest.mock('../services/auth');
+
 describe('App component', () => {
   it('renders App', () => {
     const wrapper = shallow(<App />);
     expect(wrapper).toMatchSnapshot();
   });
 });
-  
